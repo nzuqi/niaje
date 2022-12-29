354a295e2e4c2702aa08c0a635f48ec20881b2c1
Martin Nzuki <iamnzuki@gmail.com>
2022-12-29 22:29:28 +0300

    Integrated TTS & minor fixes

--- lib/src/home/home_view.dart
+++ lib/src/home/home_view.dart
@@ -1,8 +1,11 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:niaje/api/speech_api.dart';
 import 'package:niaje/widgets/sliver_custom_app_bar.dart';
 import 'package:niaje/util/common.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -12,21 +15,46 @@ class HomeView extends StatefulWidget {
   HomeViewState createState() => HomeViewState();
 }
 
+enum TtsState { playing, stopped, paused, continued }
+
 class HomeViewState extends State<HomeView> {
   final String defaultStr = 'Press the mic & start speaking...';
   late String text;
   late bool cleared;
   bool isListening = false;
 
+  // TTS ============
+  late FlutterTts flutterTts;
+  String? language;
+  String? engine;
+  double volume = 1;
+  double pitch = 1.0;
+  double rate = 0.5;
+  bool isCurrentLanguageInstalled = false;
+
+  TtsState ttsState = TtsState.stopped;
+
+  get isPlaying => ttsState == TtsState.playing;
+  get isStopped => ttsState == TtsState.stopped;
+  get isPaused => ttsState == TtsState.paused;
+  get isContinued => ttsState == TtsState.continued;
+
+  bool get isIOS => !kIsWeb && Platform.isIOS;
+  bool get isAndroid => !kIsWeb && Platform.isAndroid;
+  bool get isWindows => !kIsWeb && Platform.isWindows;
+  bool get isWeb => kIsWeb;
+
   @override
   void initState() {
     cleared = true;
     text = defaultStr;
+    initTts();
     super.initState();
   }
 
   @override
   void dispose() {
+    flutterTts.stop();
     super.dispose();
   }
 
@@ -98,23 +126,131 @@ class HomeViewState extends State<HomeView> {
   }
 
   Future toggleRecording() {
-    return SpeechApi.toggleRecording(
-      onResult: (text) {
-        this.text = text;
-        cleared = false;
-        setState(() {});
-      },
-      onListening: (isListening) {
-        this.isListening = isListening;
-        setState(() {});
+    try {
+      return SpeechApi.toggleRecording(
+        onResult: (text) {
+          this.text = text;
+          cleared = false;
+          setState(() {});
 
-        if (!isListening) {
-          Future.delayed(const Duration(seconds: 1), () {
-            logger.i(text);
-            // Make request to ChatGPT here...
-          });
-        }
-      },
-    );
+          if (!isListening) {
+            Future.delayed(const Duration(seconds: 1), () {
+              logger.i(text);
+              // Make request to ChatGPT here...
+              // Read out loud API response
+              _speak();
+            });
+          }
+        },
+        onListening: (isListening) {
+          setState(() => this.isListening = isListening);
+        },
+      );
+    } catch (e) {
+      text = defaultStr;
+      cleared = true;
+      setState(() {});
+      logger.e(e);
+      rethrow;
+    }
+  }
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    _setAwaitOptions();
+
+    if (isAndroid) {
+      _getDefaultEngine();
+      _getDefaultVoice();
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        logger.d("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    if (isAndroid) {
+      flutterTts.setInitHandler(() {
+        setState(() {
+          logger.d("TTS Initialized");
+        });
+      });
+    }
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        logger.d("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        logger.d("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setPauseHandler(() {
+      setState(() {
+        logger.d("Paused");
+        ttsState = TtsState.paused;
+      });
+    });
+
+    flutterTts.setContinueHandler(() {
+      setState(() {
+        logger.d("Continued");
+        ttsState = TtsState.continued;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        logger.d("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _getDefaultEngine() async {
+    var engine = await flutterTts.getDefaultEngine;
+    if (engine != null) {
+      logger.d(engine);
+    }
+  }
+
+  Future _getDefaultVoice() async {
+    var voice = await flutterTts.getDefaultVoice;
+    if (voice != null) {
+      logger.d(voice);
+    }
+  }
+
+  Future _speak() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (text.isNotEmpty) {
+      await flutterTts.speak(text);
+    }
+  }
+
+  Future _setAwaitOptions() async {
+    await flutterTts.awaitSpeakCompletion(true);
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  Future _pause() async {
+    var result = await flutterTts.pause();
+    if (result == 1) setState(() => ttsState = TtsState.paused);
   }
 }
